a8cbe1a01d3313310bb8730f64257f6f7a2d4d85
Administrator <123456>
2018-11-19 11:52:26 +0800

    add provider

---
+++ geetolsdk/src/main/java/com/gtdev5/geetolsdk/mylibrary/widget/GeetolProvider.java
@@ -0,0 +1,10 @@
+package com.gtdev5.geetolsdk.mylibrary.widget;
+
+import android.support.v4.content.FileProvider;
+
+/**
+ * Created by Walter on 2018/11/19.
+ */
+
+public class GeetolProvider extends FileProvider {
+}
